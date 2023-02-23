7b58cdfb9d05cd3f6178ab3950a56f98871c188d
burakcelen@starkajans.com <burakcelen17@gmail.com>
2023-02-23 20:21:05 +0300

    one signal

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from 'axios'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -9,6 +10,31 @@ import withUUID from "vue-uuid";
 import moment from "moment";
 import Notifications from '@kyvg/vue3-notification'
 
+
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+	OneSignal.init({
+		appId: "2d36f7bf-0670-46e7-8f58-a738237a3b3e",
+	});
+});
+
+OneSignal.registerForPushNotifications({
+	modalPrompt: true,
+})
+
+
+const headers = {
+	'Authorization': 'Basic 2d36f7bf-0670-46e7-8f58-a738237a3b3e',
+	'Content-Type': 'application/json',
+}
+
+const data = {
+	app_id: '2d36f7bf-0670-46e7-8f58-a738237a3b3e',
+	contents: { en: 'Hello, world!' },
+	included_segments: ['Subscribed Users'],
+}
+
+axios.post('https://onesignal.com/api/v1/notifications', data, { headers })
 
 const app = createApp(App)
 app.use(store)
